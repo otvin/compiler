b7d2765ddc1a9c78d382dbe5a3966019062d8aa9
otvin <michael_fredericks@hotmail.com>
2018-05-13 21:28:26 -0400

    Initial commit
    Handles integer math expressions using addition, subtraction, multiplication, integer division, and parentheses with proper order of operations.
    
    Handles Unary minus (but only one.  So -1 is valid; ---1 is a syntax error).
    
    Generates .asm file, compiles, and links.

---
+++ asm_funcs.py
@@ -0,0 +1,164 @@
+import os
+
+
+class Assembler:
+    def __init__(self, asm_filename):
+        self.asm_file = open(asm_filename,'w')
+
+    def emit(self, s):
+        self.asm_file.write(s)
+
+    def emitln(self, s):
+        self.emit(s + '\n')
+
+    def emitcode(self,s):
+        self.emitln('\t' + s)
+
+    def emitlabel(self,s):
+        self.emitln(s)
+
+    def cleanup(self):
+        self.asm_file.close()
+
+    def setup_bss(self):
+        self.emitlabel("section .bss")
+        self.emitcode("write_intDigitSpace resb 100")
+        self.emitcode("write_intDigitSpacePos resb 8")
+
+    def setup_data(self):
+        self.emitlabel("section .data")
+        self.emitcode('CRLF db 10,0')
+        self.emitcode('minusSign db "-"')
+
+    def setup_text(self):
+        self.emitlabel("section .text")
+        self.emitcode("global _start")
+
+    def setup_start(self):
+        self.emitlabel("_start:")
+
+    def emit_terminate(self):
+        self.emitcode("mov rax,60")
+        self.emitcode("mov rdi,0")
+        self.emitcode("syscall")
+
+    def emit_writeINT(self):
+        self.emitlabel("_writeINT:\t\t;int to be written must be in rax") # todo figure out how to preserve RAX if it is negative
+        self.emitlabel("; source = https://www.youtube.com/watch?v=XuUD0WQ9kaE - modified to do negatives")
+        self.emitcode("cmp rax, 0")
+        self.emitcode("jge .doneSigned")
+        self.emitcode("; need to print the minus sign and negate RAX")
+        self.emitcode("push rax")
+        self.emitcode("mov rax, 1")
+        self.emitcode("mov rdi, 1")
+        self.emitcode("mov rsi, minusSign")
+        self.emitcode("mov rdx, 19")
+        self.emitcode("syscall")
+        self.emitcode("pop rax")
+        self.emitcode("neg rax")
+
+
+        self.emitlabel(".doneSigned:")
+        self.emitcode("mov rcx, write_intDigitSpace")
+        self.emitcode("mov [write_intDigitSpacePos], rcx")
+        self.emitlabel(".writeINTLoop:")
+        self.emitcode("mov rdx, 0\t\t;because rdx gets concatenated onto rax when calling div")
+        self.emitcode("mov rbx, 10")
+        self.emitcode("div rbx")
+        self.emitcode("push rax")
+        self.emitcode("add rdx, 48")
+        self.emitcode("mov rcx, [write_intDigitSpacePos]")
+        self.emitcode("mov [rcx], dl")
+        self.emitcode("inc rcx")
+        self.emitcode("mov [write_intDigitSpacePos], rcx")
+        self.emitcode("pop rax")
+        self.emitcode("cmp rax, 0")
+        self.emitcode("jne .writeINTLoop")
+        self.emitlabel(".writeINTLoop2:")
+        self.emitcode("mov rcx, [write_intDigitSpacePos]")
+        self.emitcode("mov rax, 1")
+        self.emitcode("mov rdi, 1")
+        self.emitcode("mov rsi, rcx")
+        self.emitcode("mov rdx, 1")
+        self.emitcode("syscall")
+        self.emitcode("mov rcx, [write_intDigitSpacePos]")
+        self.emitcode("dec rcx")
+        self.emitcode("mov [write_intDigitSpacePos], rcx")
+        self.emitcode("cmp rcx, write_intDigitSpace")
+        self.emitcode("jge .writeINTLoop2")
+        self.emitcode("ret")
+
+    def emit_writeCRLF(self):
+        self.emitlabel("_writeCRLF:")
+        self.emitcode("mov rax, 1")
+        self.emitcode("mov rdi, 1")
+        self.emitcode("mov rsi, CRLF")
+        self.emitcode("mov rdx, 1")
+        self.emitcode("syscall")
+        self.emitcode("ret")
+
+    def emit_writeSTR(self):
+        pass
+
+    def emit_systemfunctions(self):
+        self.emit_writeINT()
+        self.emit_writeSTR()
+        self.emit_writeCRLF()
+
+class ASTAssembler:
+    def __init__(self, AST, assembler):
+        self.AST = AST
+        self.assembler = assembler
+
+    def assemble(self):
+        pass
+
+class Compiler:
+    def __init__(self, asm_filename, obj_filename):
+        self.asm_filename = asm_filename
+        self.obj_filename = obj_filename
+        
+    def do_compile(self):
+        os.system("nasm -f elf64 -o " + self.obj_filename + " " + self.asm_filename)
+
+class Linker:
+    def __init__(self, obj_filename, exe_filename):
+        self.obj_filename = obj_filename
+        self.exe_filename = exe_filename
+        
+    def do_link(self):
+        os.system("ld " + self.obj_filename + " -o " + self.exe_filename)
+
+
+
+def DEBUG_helloworld_test():
+    a = Assembler("mytest.asm")
+
+    a.emitlabel('section .data')
+    a.emitcode('text db "Hello, World!", 10')
+    a.emitln("")
+
+    a.emitlabel('section .text')
+    a.emitcode('global _start')
+    a.emitln("")
+
+    a.emitlabel('_start:')
+    a.emitlabel('mov rax, 1')
+    a.emitlabel('mov rdi, 1')
+    a.emitlabel('mov rsi, text')
+    a.emitlabel('mov rdx, 14')
+    a.emitlabel('syscall')
+    a.emitln("")
+
+    a.emitlabel('mov rax, 60')
+    a.emitlabel('mov rdi, 0')
+    a.emitlabel('syscall')
+
+    a.cleanup()
+
+    c = Compiler("mytest.asm", "mytest.o")
+    c.do_compile()
+
+    l = Linker("mytest.o","mytest")
+    l.do_link()
+
---
+++ compiler.py
@@ -0,0 +1,272 @@
+import asm_funcs
+
+# constants for token types
+TOKEN_INT = 0
+TOKEN_PLUS = 1
+TOKEN_MINUS = 2
+TOKEN_MULT = 3
+TOKEN_IDIV = 4
+TOKEN_LPAREN = 5
+TOKEN_RPAREN = 6
+
+# hack for pretty printing
+DEBUG_TOKENDISPLAY = ['INT','+','-','*','/','(',')']
+
+# helper functions
+def isSymbol(char):
+	if char in ["-","+","(",")","*","/"]:
+		return True
+	else:
+		return False
+
+
+# grammar for now is just evaluating the expression
+
+
+# expression ::= <term> [ <addop> <term>]*
+# term ::= <factor> [ <multop> <factor>]*
+# factor ::== <integer> | <lparen> <expression> <rparen>
+# addop = + | -
+# multop = * | /
+
+class Token:
+	def __init__(self, type, value):
+		self.type = type
+		self.value = value
+
+	def debugprint(self):
+		return(DEBUG_TOKENDISPLAY[self.type] + ":" + str(self.value))
+
+
+class AST():
+	def __init__(self, token):
+		self.token = token
+		self.children = []
+
+	def rpn_print(self):
+		for x in self.children:
+			x.rpn_print()
+		print(self.token.debugprint())
+
+	def interpret(self):
+		if self.token.type == TOKEN_INT:
+			return self.token.value
+		elif self.token.type == TOKEN_PLUS:
+			return self.children[0].interpret() + self.children[1].interpret()
+		elif self.token.type == TOKEN_MINUS:
+			return self.children[0].interpret() - self.children[1].interpret()
+		elif self.token.type == TOKEN_MULT:
+			return self.children[0].interpret() * self.children[1].interpret()
+		elif self.token.type == TOKEN_IDIV:
+			return self.children[0].interpret() // self.children[1].interpret()
+		else:
+			raise ValueError("Unexpected Token")
+
+	def assemble(self, assembler):
+		if self.token.type == TOKEN_INT:
+			assembler.emitcode("MOV RAX, " + str(self.token.value))
+		elif self.token.type == TOKEN_PLUS:
+			self.children[0].assemble(assembler)
+			# RAX now contains value of the first child
+			assembler.emitcode("PUSH RAX")
+			self.children[1].assemble(assembler)
+			# RAX now contains value of the second child
+			assembler.emitcode("POP RCX")
+			# RCX now contains value of the first child
+			assembler.emitcode("ADD RAX, RCX")
+		elif self.token.type == TOKEN_MINUS:
+			self.children[0].assemble(assembler)
+			assembler.emitcode("PUSH RAX")
+			self.children[1].assemble(assembler)
+			assembler.emitcode("POP RCX")
+			assembler.emitcode("SUB RCX, RAX")
+			assembler.emitcode("MOV RAX, RCX")  # it might be quicker to sub RAX, RCX and NEG RAX.
+		elif self.token.type == TOKEN_MULT:
+			self.children[0].assemble(assembler)
+			assembler.emitcode("PUSH RAX")
+			self.children[1].assemble(assembler)
+			assembler.emitcode("POP RCX")
+			assembler.emitcode("IMUL RAX, RCX")
+		elif self.token.type == TOKEN_IDIV:
+			self.children[0].assemble(assembler)
+			assembler.emitcode("PUSH RAX")
+			self.children[1].assemble(assembler)
+			assembler.emitcode("MOV RCX, RAX")
+			assembler.emitcode("POP RAX")
+			assembler.emitcode("XOR RDX, RDX")  # RDX is concatenated with RAX to do division
+			assembler.emitcode("IDIV RCX")
+		else:
+			raise ValueError("Unexpected Token")
+
+
+class Tokenizer:
+	def __init__(self, text):  # todo - pull from file
+		self.curPos = 0
+		self.text = text
+		self.length = len(text)
+
+	def peek(self):
+		if self.curPos >= self.length:
+			return ""
+		else:
+			return self.text[self.curPos]
+
+	def eat(self):
+		if self.curPos >= self.length:
+			raise ValueError("Length Exceeded")
+		else:
+			retChar = self.text[self.curPos]
+			self.curPos += 1
+			return retChar
+
+	def getIdentifier(self):
+		if self.peek().isalpha():
+			retVal = self.eat()
+			while self.peek().isalnum():
+				retVal += self.eat()
+			return retVal
+		else:
+			raise ValueError("Identifiers must begin with alpha character")  # todo - error should indicate what was seen
+
+	def getNumber(self):
+		if self.peek().isdigit(): # todo - handle floats
+			retval = self.eat()
+			while self.peek().isdigit():
+				retval += self.eat()
+			return int(retval)
+		else:
+			raise ValueError("Numbers must be numeric")
+
+	def getSymbol(self):
+		if isSymbol(self.peek()): # todo - handle multi-character symbols
+			return self.eat()
+		else:
+			raise ValueError("Symbol Expected")
+
+	def getNextToken(self):
+		if self.curPos > self.length:
+			return None
+		else:
+			if self.peek().isalpha():
+				raise ValueError("Cannot handle identifiers yet")
+			elif self.peek().isdigit():
+				ret = Token(TOKEN_INT, self.getNumber())
+			elif isSymbol(self.peek()):
+				sym = self.getSymbol()
+				if sym == "+":
+					ret = Token(TOKEN_PLUS, None)
+				elif sym == "-":
+					ret = Token(TOKEN_MINUS, None)
+				elif sym == "/":
+					ret = Token(TOKEN_IDIV, None)
+				elif sym == "*":
+					ret = Token(TOKEN_MULT, None)
+				elif sym == "(":
+					ret = Token(TOKEN_LPAREN, None)
+				elif sym == ")":
+					ret = Token(TOKEN_RPAREN, None)
+				else:
+					raise ValueError ("Unrecognized Token: " + sym)
+
+			while self.peek().isspace():
+				self.eat()
+			return ret
+
+
+
+class Parser:
+	def __init__ (self, tokenizer):
+		self.tokenizer = tokenizer
+		self.AST = None
+		self.asssembler = None
+
+	def parseFactor(self):
+		# factor ::== <0 or 1 minus signs> <integer> | <lparen> <expression> <rparen>
+		# just do integer for now
+		if self.tokenizer.peek() == "(":
+			# parens do not go in the AST
+			lparen = self.tokenizer.getNextToken()
+			ret = self.parseExpression()
+			if self.tokenizer.peek() != ")":
+				raise ValueError("Right Paren expected")
+			rparen = self.tokenizer.getNextToken()
+			return(ret)
+		else:
+			multby = 1 # will set this negative if it's a negative number
+			factor = self.tokenizer.getNextToken()
+			if factor.type == TOKEN_MINUS:
+				multby = -1
+				factor = self.tokenizer.getNextToken()
+			if factor.type != TOKEN_INT:
+				raise ValueError ("Integer expected")
+			factor.value = factor.value * multby
+			return AST(factor)
+
+	def parseTerm(self):
+		# term ::= <factor> [ <multop> <factor>]*
+		ret = self.parseFactor()
+		while self.tokenizer.peek() in ["*","/"]:
+			multdiv = AST(self.tokenizer.getNextToken())
+			multdiv.children.append(ret)
+			multdiv.children.append(self.parseFactor())
+			ret = multdiv
+		return ret
+
+
+	def parseExpression(self):
+		# expression ::= <term> [ <addop> <term>]*
+		ret = self.parseTerm()
+		while self.tokenizer.peek() in ['+','-']:
+			addsub = AST(self.tokenizer.getNextToken())
+			addsub.children.append(ret)
+			addsub.children.append(self.parseTerm())
+			ret = addsub
+		return ret
+
+	def parse(self):
+		self.AST = self.parseExpression()
+
+	def assembleAST(self):
+		# self.assembler.emitcode("mov rax, 42")
+		self.AST.assemble(self.assembler)
+
+		self.assembler.emitcode("call _writeINT")
+		self.assembler.emitcode("call _writeCRLF")
+
+	def assemble(self, filename):
+		self.assembler = asm_funcs.Assembler(filename)
+		self.assembler.setup_bss()
+		self.assembler.setup_data()
+		self.assembler.setup_text()
+		self.assembler.setup_start()
+
+		self.assembleAST()
+
+		self.assembler.emit_terminate()
+		self.assembler.emit_systemfunctions()
+		self.assembler.cleanup()
+
+
+def main():
+
+	t = Tokenizer("-2* -7")
+	p = Parser(t)
+	p.parse()
+	p.assemble("test.asm")
+	# p.AST.rpn_print()
+
+	# print (p.AST.interpret())
+
+	c = asm_funcs.Compiler("test.asm","test.o")
+	c.do_compile()
+	l = asm_funcs.Linker("test.o","test")
+	l.do_link()
+
+	print("\n\n\n")
+
+
+
+
+if __name__ == '__main__':
+	main()
+
